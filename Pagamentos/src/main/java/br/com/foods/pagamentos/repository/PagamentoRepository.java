01ed960db93c9a2e3fcc5f069381d1903319b2ae
Vanessazanco <vanessazancof@gmail.com>
2024-08-28 12:12:12 +0100

    First commit

---
+++ Pagamentos/src/main/java/br/com/foods/pagamentos/repository/PagamentoRepository.java
@@ -0,0 +1,8 @@
+package br.com.foods.pagamentos.repository;
+
+import br.com.foods.pagamentos.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagamentoRepository  extends JpaRepository<Pagamento,Long> {
+
+}
